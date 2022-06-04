788411f56cda90d1fe86e4f70cb2d19ed180245c
dervalinor <dervalinor@protonmail.com>
2022-06-03 22:00:33 -0500

    Sleep function to automate message

--- 1_first_program_bash/Hello.sh
+++ 1_first_program_bash/Hello.sh
@@ -5,3 +5,17 @@
 #bash filename.sh --- execute script
 
 echo "Hello friend"
+
+#we can automate a message wiht sleep function:
+
+sleep 3 #show the message after of three seconds
+
+echo "I can't believe that" 
+
+sleep 3
+
+echo "you are learning to hack in linux"
+
+sleep 3
+
+echo "Congratulations friend !!!"
