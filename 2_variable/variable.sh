04c68ec4b5ac99cdaaf6e902fe9e6bd89a24ff4b
melkorval <prushka@tutanota.com>
2022-06-04 21:40:48 -0500

    What is variables and declare variable

---
+++ 2_variable/variable.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#variable: storage in memory to store a known or unknown amount of information
+#variablename = "value"
+
+Name="Elliot"
+
+#show a variable - $variablename
+echo "Hello $Name"
+
+sleep 2
+
+echo "I've told you we'd end up working together, $Name"
+
+sleep 2
+
+echo "Why did you do it, $Name"
