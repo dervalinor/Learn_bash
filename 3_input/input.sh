e1b75116c800cf826a4ec55b1b8daf69a8927f8a
dervalinor <dervalinor@protonmail.com>
2022-06-05 19:05:48 -0500

    Input data, making bash read

---
+++ 3_input/input.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#input data - read variablename
+
+#create a program, where it asks for a name and will show you a greeting
+
+echo "What is your name?: "
+
+#read - get user input,variablename - store inside the variable
+#Let's ask the user
+read name
+
+echo "Hi $name"
+
+sleep 2
+echo "Welcome to Bash $name"
+
+sleep 2
+echo "Welcome to Kali Linux $name"
+
+sleep 2
+echo "Have a nice day !!!"
