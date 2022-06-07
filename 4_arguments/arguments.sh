f1bf452bb711e346efaf43e7583e84bf23e0e163
dervalinor <dervalinor@protonmail.com>
2022-06-06 21:35:01 -0500

    Arguments and variables

---
+++ 4_arguments/arguments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#arguments - is a way for you to provide more information to a function. this case 
+#use argument to give information to a variable
+
+
+name=$1 #the variable take the value of the first argument
+
+echo "$name talks to Jocelyn about writting"
+
+sleep 2
+
+echo "$name listen to Jocelyn when she talks about your writting"
+
